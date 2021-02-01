0039579ef541da6a4be80913213d1bdc51cd9c1d
Alex Kogi <Alexandrekogi@gmail.com>
2021-02-01 15:32:59 +0100

    second test vsstudio

--- test.py
+++ test.py
@@ -7,11 +7,5 @@ import requests
 #print(sys.version)
 print(sys.executable)
 
-def greet(who_to_greet):
-    greeting = 'Hello, {}'.format(who_to_greet)
-    return greeting
-    
 r = requests.get('https://coreyms.com')
 print(r.status_code)   
-
-test 
